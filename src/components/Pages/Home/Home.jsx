bba2a9d95d4f804bb4b59993120d5bf96733779f
Your Username <magoxaire@gmail.com.com>
2025-03-19 09:05:39 -0500

    efecto a h1

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,13 +1,26 @@
+import BlurText from '../../../TextAnimations/BlurText/BlurText';
 import Img from '../../img/IMG-20240422-WA0001.jpg'
 import './Home.css'
 
 
 export const Home = () => {
+    const handleAnimationComplete = () => {
+        console.log('Animation completed!');
+      };
     return(
         <div className="Home">
             <div className="title">
             <img src={Img} alt="" className="Img"/>
-                <h2>Luis Carlos Salazar Cortes</h2>
+            <h2>
+  <BlurText
+    text="Luis Carlos Salazar Cortes"
+    delay={150}
+    animateBy="words"
+    direction="top"
+    onAnimationComplete={handleAnimationComplete}
+    className="text-2xl mb-8 flex-text" /* Add flex-text class */
+  />
+</h2>
                 <p>
                 Desarrollador FullStack & UI/UX Designer
                 </p>
