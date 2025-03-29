1502e3458f76f2bd67d380aef9fe9e579f925d14
luiscar2050 <luis3206162549@gmail.com>
2025-03-28 20:12:04 -0600

    card terminadas

--- src/components/Pages/Experience/Experience.jsx
+++ src/components/Pages/Experience/Experience.jsx
@@ -4,32 +4,32 @@ export const Experience = () => {
     return (
         <>
             <div class="card-link">
-                <div class="card__image">
+                <div class="card__image1">
                 </div>
                 <div class="card__content__oficial">
-                    <p class="card__title">Card Title</p>
-                    <p class="card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <a class="card__button" href="#">Read More</a>
+                    <p class="card__title">Reproductor</p>
+                    <p class="card__text">Este reproductor de música MP3 es una aplicación desarrollada para ofrecer una experiencia sencilla e intuitiva al reproducir archivos de audio. </p>
+                    <a class="card__button" href="https://reproductor-js-alpha.vercel.app/" target="_blank">Ir a pagina</a>
                 </div>
             </div>
 
             <div class="card-link">
-                <div class="card__image">
+                <div class="card__image2">
                 </div>
                 <div class="card__content__oficial">
-                    <p class="card__title">Card Title</p>
-                    <p class="card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <a class="card__button" href="#">Read More</a>
+                    <p class="card__title">E-Comerce</p>
+                    <p class="card__text">Esta tienda en línea ofrece una experiencia de compra moderna y sencilla para encontrar las últimas tendencias en moda.</p>
+                    <a class="card__button" href="https://project-ropa-blush.vercel.app/" target="_blank">Ir a pagina</a>
                 </div>
             </div>
 
             <div class="card-link">
-                <div class="card__image">
+                <div class="card__image3">
                 </div>
                 <div class="card__content__oficial">
-                    <p class="card__title">Card Title</p>
-                    <p class="card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <a class="card__button" href="#">Read More</a>
+                    <p class="card__title">Diccionario</p>
+                    <p class="card__text">Este diccionario en línea permite a los usuarios traducir palabras entre inglés y español de manera rápida y sencilla. Además de proporcionar significados y ejemplos de uso.</p>
+                    <a class="card__button" href="https://dictionary-ten-coral.vercel.app/" target="_blank">Ir a pagina</a>
                 </div>
             </div>
 
--- src/components/Pages/TechnicalSkills/TechnicalSkills.jsx
+++ src/components/Pages/TechnicalSkills/TechnicalSkills.jsx
@@ -9,11 +9,8 @@ export const TechnicalSkills = () => {
     <div class="card-title1">MIS HABILIDADES</div>
     <div class="card-description1">Poseo conocimientos fundamentales en HTML, CSS, JavaScript, Java, MongoDb, PHP y Python, así como experiencia en el manejo de bases de datos y frameworks de diferentes lenguajes. Estas habilidades técnicas me permiten desarrollar y diseñar soluciones web dinámicas y funcionales</div>
     
-    <div class="card-title1">EDUCACIÓN</div>
-    <div class="card-description1">2021-2023 Roman Maria Valencia (Calarcá/Quindio)</div>
-  
-    <div class="card-title1">IDIOMAS</div>
-    <div class="card-description1">ESPAÑOL - INGLES(BÁSICO)</div>
+    <div class="card-title1">EXPERIENCIA</div>
+    <div class="card-description1">Poseo experiencia significativa en el desarrollo de páginas web utilizando HTML, REACT, JAVASCRIPT, JAVA, CSS y PHP, incluyendo la creación de operaciones CRUD (Crear, Leer, Actualizar, Eliminar) para interactuar con bases de datos. Esta experiencia me ha permitido no solo comprender los fundamentos de la creación de sitios web, sino también implementar funcionalidades dinámicas y sistemas de gestión de contenido. </div>
   
   </div>
 </div>
