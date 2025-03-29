61220af8a203da81a74a2b44a934b23e1ae23558
luiscar2050 <luis3206162549@gmail.com>
2025-03-28 19:39:08 -0600

    cards

--- src/components/Pages/Experience/Experience.jsx
+++ src/components/Pages/Experience/Experience.jsx
@@ -1,9 +1,38 @@
-
+import "./experience.css";
 
 export const Experience = () => {
     return (
         <>
-        <h2>hola soy Experience</h2>
+            <div class="card-link">
+                <div class="card__image">
+                </div>
+                <div class="card__content__oficial">
+                    <p class="card__title">Card Title</p>
+                    <p class="card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <a class="card__button" href="#">Read More</a>
+                </div>
+            </div>
+
+            <div class="card-link">
+                <div class="card__image">
+                </div>
+                <div class="card__content__oficial">
+                    <p class="card__title">Card Title</p>
+                    <p class="card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <a class="card__button" href="#">Read More</a>
+                </div>
+            </div>
+
+            <div class="card-link">
+                <div class="card__image">
+                </div>
+                <div class="card__content__oficial">
+                    <p class="card__title">Card Title</p>
+                    <p class="card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <a class="card__button" href="#">Read More</a>
+                </div>
+            </div>
+
         </>
     )
 }
